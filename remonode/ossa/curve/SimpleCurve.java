92565fac995f09953cd747eadfc8584a32fe3db6
AdetonaMichael <powerofelectron616@gmail.com>
2024-05-20 00:49:49 +0100

    Test runing the Momento design Pattern

--- remonode/ossa/curve/SimpleCurve.java
+++ remonode/ossa/curve/SimpleCurve.java
@@ -1,6 +1,11 @@
 package remonode.ossa.curve;
+
+import remonode.ossa.momento.Editor;
+import remonode.ossa.momento.History;
+
 /**
  * SimpleCurve
+ * With Undo Functionality to track state of previous coordinate
  */
 public class SimpleCurve implements Curve {
 
@@ -15,26 +20,39 @@ public class SimpleCurve implements Curve {
     public double chainageAtPointTangent;
     public double chainageAtPointCurve;
 
+    Editor  editor = new Editor();
+    History history = new History();
+
     public SimpleCurve(double radiusInput, double deflectionAngleInput){
         radius = radiusInput;
         deflectionAngle = deflectionAngleInput;
         tangentLength = radiusInput*Math.tan(Math.toRadians(deflectionAngleInput/2));
+        editor.setContent("Simple Curve Calculated");
+        history.push(editor.createState());
     }
 
     public void calculateMidOrdinate(double radiusInput, double deflectionAngleInput){
         midOrdinate = radiusInput*(1-Math.cos(Math.toRadians(deflectionAngleInput/2)));
+        editor.setContent("Mid-Ordinate Calculated.");
+        history.push(editor.createState());
     }
 
     public void calculateLongChord(double radiusInput, double deflectionAngleInput){
         longChord = 2*radiusInput*(Math.sin(Math.toRadians(deflectionAngleInput/2)));
+        editor.setContent("Longchord Calculated");
+        history.push(editor.createState());
     }
 
     public void calculateExternalDistance(double radiusInput, double deflectionAngleInput){
         externalDistance = radiusInput *(Math.cosh(Math.toRadians(deflectionAngleInput/2))-1);
+        editor.setContent("External Distance Calculated");
+        history.push(editor.createState());
     }
 
     public void calculateCurveLength(double radiusInput, double deflectionAngleInput){
         curveLength = (deflectionAngleInput* 2*Math.PI*radiusInput)/360;
+        editor.setContent("CurveLength Calculated");
+        history.push(editor.createState());
     }
 
     public void showCurve(){
@@ -48,6 +66,7 @@ public class SimpleCurve implements Curve {
         System.out.printf("Chainage At Intersection: %.3f\n",chainageAtIntersectionPoint);
         System.out.printf("Chainage At Point-Tangent: %.3f\n",chainageAtPointTangent);
         System.out.printf("Chainage At Point-Curve: %.3f\n",chainageAtPointCurve);
+        System.out.println(editor.getContent());
     }
 
     public void calculateChainage(int chainageType, double chainageValue){
